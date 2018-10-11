362c283a7a29418ed03230f7b0a126ee6f2dc007
Bryan Cairns <bcairns@voidrealms.com>
2018-10-11 14:41:09 -0400

    first commit

---
+++ source.h
@@ -0,0 +1,20 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <QObject>
+
+class source : public QObject
+{
+    Q_OBJECT
+public:
+    explicit source(QObject *parent = nullptr);
+
+    void test();
+
+signals:
+    void mySignal(QString message);
+
+public slots:
+};
+
+#endif // SOURCE_H
